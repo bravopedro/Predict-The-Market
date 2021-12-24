f7253b7364340a4a799ab88b4715ce250d1fe205
bravopedro <bravpj20@wfu.edu>
2021-12-24 00:18:04 -0500

    Initial commit

---
+++ src/main/java/SendMail.java
@@ -0,0 +1,25 @@
+
+
+public class SendMail {
+    //Set up Mail Server Properties
+    //Draft An Email
+    //Send Email
+    public static void main(String[] args) {
+        SendMail mail = new mail();
+        mail.setupServerProperties();
+        mail.draftEmail();
+        mail.sendEmail();
+
+    }
+    private void draftEmail(){
+
+    }
+    private void setupServerProperties(){
+
+    }
+    private void  sendEmail(){
+
+    }
+}
+
+
